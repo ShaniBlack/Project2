83277273e441dd2ab5631479d30782af6a786c18
Shani Black <shaniblack2004@gmail.com>
2020-12-10 14:59:21 -0600

    fixed errors bc of travis

--- formidable.js
+++ formidable.js
@@ -2,28 +2,28 @@ const http = require('http')
 const formidable = require('formidable')
 const fs = require('fs')
 
-http.createServer(function(req, res) {
-    if (req.url === '/fileupload') {
-        const form = new formidable.IncomingForm()
-        form.parse(req, function(err, fields, files) {
-            const oldpath = files.filetoupload.path
-            const newpath = './Assets/file_uploads' + files.filetoupload.name
-            fs.rename(oldpath, newpath, function(err) {
-                if (err) throw err
-                res.write('File uploaded and moved!')
-                res.end()
-            })
-        })
-    } else {
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        })
-        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">')
-        res.write('<input type="file" name="filetoupload"><br>')
-        res.write('<input type="submit">')
-        res.write('</form>')
-        return res.end()
-    }
+http.createServer(function (req, res) {
+  if (req.url === '/fileupload') {
+    const form = new formidable.IncomingForm()
+    form.parse(req, function (_err, fields, files) {
+      const oldpath = files.filetoupload.path
+      const newpath = './Assets/file_uploads' + files.filetoupload.name
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err
+        res.write('File uploaded and moved!')
+        res.end()
+      })
+    })
+  } else {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    })
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">')
+    res.write('<input type="file" name="filetoupload"><br>')
+    res.write('<input type="submit">')
+    res.write('</form>')
+    return res.end()
+  }
 }).listen(9000)
 
 // const express = require('express');
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -57,5 +57,4 @@ module.exports = function (app) {
   app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '../public/welcome.html'))
   })
-
 }
