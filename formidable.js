de5fbf124fb4e288bb8dec0920ca4a3ef8d33319
David Harris <jdh.programmer@gmail.com>
2020-12-10 14:00:51 -0600

    ran linting

--- formidable.js
+++ formidable.js
@@ -1,28 +1,59 @@
-var http = require('http');
-var formidable = require('formidable');
-var fs = require('fs');
-
+const http = require('http')
+const formidable = require('formidable')
+const fs = require('fs')
 
 http.createServer(function(req, res) {
-    if (req.url == '/fileupload') {
-        var form = new formidable.IncomingForm();
+    if (req.url === '/fileupload') {
+        const form = new formidable.IncomingForm()
         form.parse(req, function(err, fields, files) {
-            var oldpath = files.filetoupload.path;
-            var newpath = './Assets/file_uploads' + files.filetoupload.name;
+            const oldpath = files.filetoupload.path
+            const newpath = './Assets/file_uploads' + files.filetoupload.name
             fs.rename(oldpath, newpath, function(err) {
-                if (err) throw err;
-                res.write('File uploaded and moved!');
-                res.end();
-            });
-        });
+                if (err) throw err
+                res.write('File uploaded and moved!')
+                res.end()
+            })
+        })
     } else {
         res.writeHead(200, {
             'Content-Type': 'text/html'
-        });
-        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-        res.write('<input type="file" name="filetoupload"><br>');
-        res.write('<input type="submit">');
-        res.write('</form>');
-        return res.end();
+        })
+        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">')
+        res.write('<input type="file" name="filetoupload"><br>')
+        res.write('<input type="submit">')
+        res.write('</form>')
+        return res.end()
     }
-}).listen(9000);
+}).listen(9000)
+
+// const express = require('express');
+// const formidable = require('formidable');
+
+// const app = express();
+
+// app.get('/', (req, res) => {
+//   res.send(`
+//     <h2>With <code>"express"</code> npm package</h2>
+//     <form action="/api/upload" enctype="multipart/form-data" method="post">
+//       <div>Text field title: <input type="text" name="title" /></div>
+//       <div>File: <input type="file" name="someExpressFiles" multiple="multiple" /></div>
+//       <input type="submit" value="Upload" />
+//     </form>
+//   `);
+// });
+
+// app.post('/api/upload', (req, res, next) => {
+//   const form = formidable({ multiples: true });
+
+//   form.parse(req, (err, fields, files) => {
+//     if (err) {
+//       next(err);
+//       return;
+//     }
+//     res.json({ fields, files });
+//   });
+// });
+
+// app.listen(9000, () => {
+//   console.log('Server listening on http://localhost:9000 ...');
+// });
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,47 +10,47 @@ const isAuthenticated = require('../config/middleware/isAuthenticated')
 
 // Routes
 // =============================================================
-module.exports = function(app) {
-    app.get('/', (req, res) => {
-        // If the user already has an account send them to the members page
-        if (req.user) {
-            res.redirect('/members')
-        }
-        res.sendFile(path.join(__dirname, '../public/signup.html'))
-    })
+module.exports = function (app) {
+  app.get('/', (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect('/members')
+    }
+    res.sendFile(path.join(__dirname, '../public/signup.html'))
+  })
 
-    app.get('/login', (req, res) => {
-        // If the user already has an account send them to the members page
-        if (req.user) {
-            res.redirect('/members')
-        }
-        res.sendFile(path.join(__dirname, '../public/login.html'))
-    })
+  app.get('/login', (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect('/members')
+    }
+    res.sendFile(path.join(__dirname, '../public/login.html'))
+  })
 
-    // Here we've add our isAuthenticated middleware to this route.
-    // If a user who is not logged in tries to access this route they will be redirected to the signup page
-    app.get('/members', isAuthenticated, (req, res) => {
-            res.sendFile(path.join(__dirname, '../public/members.html'))
-        })
-        // Each of the below routes just handles the HTML page that the user gets sent to.
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get('/members', isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/members.html'))
+  })
+  // Each of the below routes just handles the HTML page that the user gets sent to.
 
-    // index route loads view.html
-    // app.get("/journal", function(req, res) {
-    //   res.sendFile(path.join(__dirname, "../public/journal.html"));
-    // });
+  // index route loads view.html
+  // app.get("/journal", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/journal.html"));
+  // });
 
-    // cms route loads cms.html
-    app.get('/post', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/post.html'))
-    })
+  // cms route loads cms.html
+  app.get('/post', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/post.html'))
+  })
 
-    // blog route loads blog.html
-    app.get('/blog', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/blog.html'))
-    })
+  // blog route loads blog.html
+  app.get('/blog', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/blog.html'))
+  })
 
-    // s route loads-manager.html
-    app.get('/travelers-travel', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/travelers-travel.html'))
-    })
+  // s route loads-manager.html
+  app.get('/travelers-travel', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/travelers-travel.html'))
+  })
 }
