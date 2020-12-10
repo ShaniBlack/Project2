4718f51388a3be8e1008bbadf1eb29a603c41f3c
Shani Black <shaniblack2004@gmail.com>
2020-12-10 13:40:43 -0600

    added bg img to login page/updated login page using bulma

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // *********************************************************************************
 // html-routes.js - this file offers a set of routes for sending users to the various html pages
 // *********************************************************************************
@@ -11,14 +10,6 @@ const isAuthenticated = require('../config/middleware/isAuthenticated')
 
 // Routes
 // =============================================================
-=======
-// Requiring path to so we can use relative routes to our HTML files
-const path = require('path')
-
-// Requiring our custom middleware for checking if a user is logged in
-const isAuthenticated = require('../config/middleware/isAuthenticated')
-
->>>>>>> 250d8332582b5c36d4443429c426503137bd8bba
 module.exports = function (app) {
   app.get('/', (req, res) => {
     // If the user already has an account send them to the members page
@@ -41,7 +32,6 @@ module.exports = function (app) {
   app.get('/members', isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, '../public/members.html'))
   })
-<<<<<<< HEAD
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
@@ -63,6 +53,4 @@ module.exports = function (app) {
   app.get('/travelers-travel', function (req, res) {
     res.sendFile(path.join(__dirname, '../public/travelers-travel.html'))
   })
-=======
->>>>>>> 250d8332582b5c36d4443429c426503137bd8bba
 }
