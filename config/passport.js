32e3d61a610bc6f303597b5047ea5babcaa02042
David Harris <jdh.programmer@gmail.com>
2020-12-11 12:22:54 -0600

    merge conflicts fixed

--- config/passport.js
+++ config/passport.js
@@ -23,8 +23,12 @@ passport.use(
             message: 'Incorrect email.'
           })
         } else if (!dbUser.validPassword(password)) {
+<<<<<<< Updated upstream
         // If there is a user with the given email, but the password the user gives us is incorrect
 
+=======
+          // If there is a user with the given email, but the password the user gives us is incorrect
+>>>>>>> Stashed changes
           return done(null, false, {
             message: 'Incorrect password.'
           })
--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
-  $.get('/api/members').then(data => {
-    $('.member-name').text(data.email)
-  })
+    // This file just does a GET request to figure out which user is logged in
+    // and updates the HTML on the page
+    $.get('/api/members').then(data => {
+        $('.member-name').text(data.email)
+    })
 })
