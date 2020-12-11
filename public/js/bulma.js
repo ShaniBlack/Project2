bc32a94334980dd4fee0b2abaee4956a9a3f907b
Shani Black <shaniblack2004@gmail.com>
2020-12-11 11:44:29 -0600

    working navbar with burger menu in blog.html

--- public/js/bulma.js
+++ public/js/bulma.js
@@ -1,6 +1,6 @@
 (function () {
     const burger = document.querySelector('.navbar-burger');
-    const menu = document.querySelector('#' + burger.dataset.target);
+    const menu = document.querySelector(".navbar-menu");
     burger.addEventListener('click', function () {
         burger.classList.toggle('is-active');
         menu.classList.toggle('is-active');
