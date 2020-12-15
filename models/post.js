fd127e6eb9fe9a9a273f4638aa86d86b4377051d
David Harris <jdh.programmer@gmail.com>
2020-12-15 11:07:11 -0600

    posting successfully

--- models/post.js
+++ models/post.js
@@ -2,10 +2,15 @@ module.exports = function(sequelize, DataTypes) {
     var Post = sequelize.define("Post", {
         title: {
             type: DataTypes.STRING,
-            allowNull: false,
+            allowNull: true,
             validate: {
                 len: [1]
             }
+        },
+        country: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            len: [1]
         },
         city: {
             type: DataTypes.TEXT,
@@ -22,10 +27,18 @@ module.exports = function(sequelize, DataTypes) {
             allowNull: false,
             len: [1]
         },
+        activities: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            len: [1]
+        },
         body: {
             type: DataTypes.TEXT,
             allowNull: false,
             len: [1]
+        },
+        imgTitle: {
+            type: DataTypes.STRING
         },
         imageURL: {
             type: DataTypes.STRING
