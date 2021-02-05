f1a96275a11aa1c9a6fb79a2f5630c46254951fd
David Harris <jdh.programmer@gmail.com>
2021-02-05 10:42:37 -0600

    index to js requires were switched to ES6 imports. and heroku didn't like it. changed them back to Requires.

--- models/index.js
+++ models/index.js
@@ -1,16 +1,9 @@
 'use strict';
 
-import {
-    readdirSync
-} from 'fs';
-import {
-    basename as _basename,
-    join
-} from 'path';
-import Sequelize, {
-    DataTypes
-} from 'sequelize';
-const basename = _basename(__filename);
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
